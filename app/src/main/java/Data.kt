57f2616719777e33dfcd63fadc42b3b3762d60e1
Alfandi Alamshach <fandixpc1@gmail.com>
2024-10-18 03:19:13 +0700

    menambahkan komponen fontweight pada text Login untuk merubah text menjadi bold

--- app/src/main/java/Data.kt
+++ app/src/main/java/Data.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -36,7 +37,7 @@ fun DataDiri(modifier: Modifier = Modifier) {
             .background(color = Color(0xFF00A3E0))
     )
     {
-        Text("Login", fontSize = 50.sp)
+        Text("Login", fontSize = 50.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.padding(5.dp))
         Text("Ini adalah halaman login", fontSize = 30.sp)
         Spacer(modifier = Modifier.padding(5.dp))
@@ -46,7 +47,7 @@ fun DataDiri(modifier: Modifier = Modifier) {
             modifier = Modifier.clip(CircleShape).size(200.dp)
         )
         Spacer(modifier = Modifier.padding(5.dp))
-        Text("Nama", fontSize = 60.sp)
+        Text("Nama", fontSize = 50.sp)
         Spacer(modifier = Modifier.padding(5.dp))
         Text("Alfandi Alamshach", fontSize = 40.sp)
         Spacer(modifier = Modifier.padding(5.dp))
