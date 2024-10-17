48bdd814ea148276abd1e7f0e266f4d10efc136e
Alfandi Alamshach <fandixpc1@gmail.com>
2024-10-18 02:49:04 +0700

    menambahakan komponen spacer untuk memberi jarak dari text ini adalah halaman login dengan gambar dibawahnya

--- app/src/main/java/Data.kt
+++ app/src/main/java/Data.kt
@@ -32,6 +32,7 @@ fun DataDiri(modifier: Modifier = Modifier) {
         Text("Login", fontSize = 50.sp)
         Spacer(modifier = Modifier.padding(5.dp))
         Text("Ini adalah halaman login", fontSize = 30.sp)
+        Spacer(modifier = Modifier.padding(5.dp))
         Image(
             painter = painterResource(id = R.drawable.img_1),
             contentDescription = "",
