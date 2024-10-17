51b4225467ea6d1b974d3c4d75e3c8645bbd660f
Alfandi Alamshach <fandixpc1@gmail.com>
2024-10-18 02:18:11 +0700

    menambahkan Nama, Alfandi Alamshach dengan menggunakan komponen Text

--- app/src/main/java/Data.kt
+++ app/src/main/java/Data.kt
@@ -4,6 +4,7 @@ package com.example.project2
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
@@ -14,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -26,11 +28,16 @@ fun DataDiri(modifier: Modifier = Modifier) {
             .padding(top = 50.dp)
     )
     {
+
         Image(
             painter = painterResource(id = R.drawable.img_1),
             contentDescription = "",
             modifier = Modifier.clip(CircleShape)
         )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text("Nama", fontSize = 80.sp)
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text("Alfandi Alamshach", fontSize = 30.sp)
     }
 
 }
