7f4d686c728440aea783976c12de66c4e6d5529b
Alfandi Alamshach <fandixpc1@gmail.com>
2024-10-18 01:56:22 +0700

    Membuat file Data.kt, dan didalam file tersebut berisi fungsi DataDiri, dalam fungsi DataDiri terdapat componen layout column

---
+++ app/src/main/java/Data.kt
@@ -0,0 +1,27 @@
+
+package com.example.project2
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun DataDiri(modifier: Modifier = Modifier) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)
+    )
+    {
+
+    }
+
+}
